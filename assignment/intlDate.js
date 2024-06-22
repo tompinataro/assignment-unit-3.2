f5f4ef7addcd1705630d7a531f879b8fb9024e2c
Tom Pinataro <tompinataro@gmail.com>
2024-06-22 15:15:51 -0500

    intlDate

---
+++ assignment/intlDate.js
@@ -0,0 +1,5 @@
+const formatter = new Intl.DateTimeFormat
+// const formatter = new Intl.RelativeTimeFormat
+// const formatter = new Intl.NumberFormat
+// const formatter = new Intl.PluralFormat
+//
